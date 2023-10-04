908534963101f3e63e019706dde5be4396c3ba8c
Dramos02 <ramosdominic0420@gmail.com>
2023-10-04 08:12:59 +0800

    2nd Commit - Functionality of the BMI App

--- app/src/main/java/com/example/bmi_app/BMIOutputScreen.kt
+++ app/src/main/java/com/example/bmi_app/BMIOutputScreen.kt
@@ -1,14 +1,63 @@
 package com.example.bmi_app
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.bmi_app.databinding.ActivityBmioutputScreenBinding
+import android.graphics.Color
 
 class BMIOutputScreen : AppCompatActivity() {
     private lateinit var binding: ActivityBmioutputScreenBinding
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityBmioutputScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        val firstname = intent.getStringExtra("firstName")
+        val lastname = intent.getStringExtra("lastName")
+        val middleInitial = intent.getStringExtra("middleInitial")
+        val gender = intent.getStringExtra("gender")
+        val bmi = intent.getFloatExtra("bmi", 0.0f) // Extract BMI as a float, defaulting to 0.0f if not found
+
+        val fullName = "$firstname $middleInitial $lastname"
+        val formattedName = fullName.split(" ").joinToString(" ") { it.capitalize() }
+
+        binding.genderTxt.text = gender
+        binding.nameTxt.text = formattedName
+        binding.bmiTxt.text = String.format("%.2f", bmi)
+
+        val bmiStatus: String = when {
+            bmi < 18.5 -> {
+                binding.bmiTagTxt.setTextColor(Color.RED) // Change color to red
+                binding.underweightTxt.setBackgroundColor(Color.RED)
+                "Underweight"
+            }
+            bmi < 25 -> {
+                binding.bmiTagTxt.setTextColor(Color.GREEN) // Change color to green
+                binding.hlthyWeightTxt.setBackgroundColor(Color.GREEN)
+                "Healthy Weight"
+            }
+            bmi < 30 -> {
+                binding.bmiTagTxt.setTextColor(Color.RED) // Change color to red
+                binding.overweightTxt.setBackgroundColor(Color.RED)
+                "Overweight"
+            }
+            else -> {
+                binding.bmiTagTxt.setTextColor(Color.RED) // Change color to red
+                binding.obeseTxt.setBackgroundColor(Color.RED)
+                "Obese"
+            }
+
+
+        }
+        binding.bmiTagTxt.text = bmiStatus
+        binding.goBackBtn.setOnClickListener {
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            this.finish()
+        }
     }
 }
--- app/src/main/java/com/example/bmi_app/MainActivity.kt
+++ app/src/main/java/com/example/bmi_app/MainActivity.kt
@@ -4,20 +4,90 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.bmi_app.databinding.ActivityMainBinding
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var selectedGender: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+        binding.maleRadBtn.setOnClickListener {
+            if (binding.maleRadBtn.isChecked) {
+                selectedGender = "Mr"
+                binding.femaleRadBtn.isChecked = false
+
+            } else {
+                binding.maleRadBtn.isChecked = true
+            }
+        }
+
+        binding.femaleRadBtn.setOnClickListener {
+            if (binding.femaleRadBtn.isChecked) {
+                selectedGender = "Ms"
+                binding.maleRadBtn.isChecked = false
+            } else {
+                binding.femaleRadBtn.isChecked = true
+            }
+        }
+
         binding.resultBtn.setOnClickListener {
-            var intent = Intent(this,BMIOutputScreen::class.java)
-            startActivity(intent)
-            this.finish()
+
+            val firstName = binding.FnameTxt.text.toString()
+            val lastName = binding.LnameTxt.text.toString()
+            val middleInitial = binding.InitialTxt.text.toString()
+            val heightStr = binding.heightInt.text.toString()
+            val weightStr = binding.weightInt.text.toString()
+
+
+
+            if (firstName.isNotEmpty() && lastName.isNotEmpty() && middleInitial.isNotEmpty() &&
+                heightStr.isNotEmpty() && weightStr.isNotEmpty()) {
+                val height = heightStr.toFloat()
+                val weight = weightStr.toFloat()
+
+                if (selectedGender == null) {
+                    Toast.makeText(applicationContext, "Please select a gender", Toast.LENGTH_SHORT).show()
+                    return@setOnClickListener // Exit the click action
+                }
+                val bmi = calcBMI(height, weight)
+
+                var intent = Intent(this, BMIOutputScreen::class.java)
+                intent.putExtra("firstName", firstName)
+                intent.putExtra("lastName", lastName)
+                intent.putExtra("middleInitial", middleInitial)
+                intent.putExtra("gender", selectedGender)
+                intent.putExtra("bmi", bmi)
+                startActivity(intent)
+                this.finish()
+            } else {
+                Toast.makeText(applicationContext, "Please fill out all fields", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun calcBMI(height: Float, weight: Float): Float {
+
+        val heightInMeters = height / 100 // Convert height from cm to meters
+        return if (heightInMeters > 0) {
+            weight / (heightInMeters * heightInMeters)
+        } else {
+            0.0f
         }
     }
 }
+
+        private fun calcBMI(height: Float, weight: Float): Float {
+            val heightInMeters = height / 100 // Convert height from cm to meters
+            return if (heightInMeters > 0) {
+                weight / (heightInMeters * heightInMeters)
+            } else {
+                0.0f
+            }
+        }
+
